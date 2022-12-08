63585fea63ac639c52a348c30666664affbfdf94
shubham07h <shubham1609sharma@gmail.com>
2022-12-08 21:48:19 +0530

    commit

---
+++ BasicCorePrograms/swap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class swap
+    {
+        static void Main(string[] args)
+        {
+            int a, b, temp;
+            Console.WriteLine("Input the First Number :");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the Second Number :");
+            b = int.Parse(Console.ReadLine());
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("After Swapping :");
+            Console.WriteLine("First Number : " + a);
+            Console.WriteLine("Second Number : " + b);
+            Console.Read();
+        }
+    }
+}
+    
+
