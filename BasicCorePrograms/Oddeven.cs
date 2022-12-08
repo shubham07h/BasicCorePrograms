fcb8d586511609640b1b0cfae44f8054ebd8812f
shubham07h <shubham1609sharma@gmail.com>
2022-12-08 22:17:49 +0530

    commit

---
+++ BasicCorePrograms/Oddeven.cs
@@ -0,0 +1,21 @@
+﻿
+
+class Oddeven
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("{0} is an even number.", num);
+        }
+        else
+        {
+            Console.WriteLine("{0} is an odd number.", num);
+        }
+
+        Console.ReadLine();
+    }
+}
