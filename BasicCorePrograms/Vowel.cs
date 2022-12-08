fdbe5ad3fdaab9b71ce62c10539bf0ecb68dd516
shubham07h <shubham1609sharma@gmail.com>
2022-12-08 22:46:04 +0530

    comit

---
+++ BasicCorePrograms/Vowel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Vowel
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an alphabet: ");
+            char ch = char.Parse(Console.ReadLine());
+            switch (char.ToUpper(ch))
+            {
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                    Console.WriteLine("The alphabet is a vowel");
+                    break;
+                default:
+                    Console.WriteLine("The alphabet is a consonant");
+                    break;
+            }
+        }
+    }
+}
+    
+
+
