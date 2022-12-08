c6156697351313d91f299a1f5fbfe949ff2a0f85
shubham07h <shubham1609sharma@gmail.com>
2022-12-08 22:37:13 +0530

    commit

---
+++ BasicCorePrograms/Largestamong.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+namespace BasicCorePrograms
+{
+     class Largestamong
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 3 numbers: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int largest = 0;
+            if (num1 > num2 && num1 > num3)
+            {
+                largest = num1;
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                largest = num2;
+            }
+            else
+            {
+                largest = num3;
+            }
+            Console.WriteLine("The largest number is {0}", largest);
+        }
+    }
+}
+
